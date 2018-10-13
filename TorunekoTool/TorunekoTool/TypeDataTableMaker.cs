53c5604f79fd798f25fe42881dd6a8f532b0d018
mitleidstein <mitleidstein@gmail.com>
2018-10-13 16:42:45 +0900

    feat:TYPE用DataTableMakerの実装

---
+++ TorunekoTool/TorunekoTool/TypeDataTableMaker.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorunekoTool
+{
+    public class TypeDataTableMaker
+    {
+        private DataTable Table;
+        private List<DtoType> TypeList;
+
+        public DataTable GetDataTable()
+        {
+            Table = new DataTable();
+            
+            var column1 = new DataColumn("TYPENUMBER", typeof(int));
+            var column2 = new DataColumn("TYPENAME", typeof(string));
+
+            Table.Columns.Add(column1);
+            Table.Columns.Add(column2);
+
+            DaoItem dao = new DaoItem();
+
+            try
+            {
+                dao.OpenConnection();
+                TypeList = dao.GetTypeList();
+            }
+            finally
+            {
+                dao.CloseConnection();
+            }
+
+            if (TypeList != null && TypeList.Count != 0)
+            {
+                foreach (var type in TypeList)
+                {
+                    Table.Rows.Add(
+                        new object[]
+                        {
+                            type.TypeNumber, type.TypeName
+                        });
+
+                }
+            }
+
+            return Table;
+        }
+    }
+}
