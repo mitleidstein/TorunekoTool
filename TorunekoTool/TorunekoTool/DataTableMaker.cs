13fc652cffbc2757e52560e2a5f0a491211e036a
mitleidstein <mitleidstein@gmail.com>
2018-10-13 12:13:52 +0900

    feat:DataTableMakerを仮実装

--- TorunekoTool/TorunekoTool/DataTableMaker.cs
+++ TorunekoTool/TorunekoTool/DataTableMaker.cs
@@ -9,12 +9,54 @@ namespace TorunekoTool
 {
     public class DataTableMaker
     {
-        private DataTable dataTable;
-        private List<DtoItem> itemList;
+        private DataTable Table;
+        private List<DtoItem> ItemList;
 
-        public DataTable GetDataTable(int Type) {
+        public DataTable GetDataTable(int type) {
+            Table = new DataTable();
 
-            return null;
+
+            var column1 = new DataColumn("UNIDENTIFIEDNAME", typeof(string));
+            var column2 = new DataColumn("NAME", typeof(string));
+            var column3 = new DataColumn("MONEYTOBUY", typeof(int));
+            var column4 = new DataColumn("MONEYTOSELL", typeof(int));
+            var column5 = new DataColumn("NOTE", typeof(string));
+
+            Table.Columns.Add(column1);
+            Table.Columns.Add(column2);
+            Table.Columns.Add(column3);
+            Table.Columns.Add(column4);
+            Table.Columns.Add(column5);
+
+            DaoItem dao = new DaoItem();
+
+            try
+            {
+                dao.OpenConnection();
+                ItemList = dao.GetItemList(
+                    new DtoItem()
+                    {
+                        TypeNumber = type
+                    });
+            }
+            finally
+            {
+                dao.CloseConnection();
+            }
+
+            if (ItemList != null && ItemList.Count != 0)
+            {
+                foreach (var item in ItemList) {
+                    Table.Rows.Add(
+                        new object[]
+                        {
+                            item.UnidentifiedName, item.Name, item.MoneyToBuy, item.MoneyToSell, item.Note
+                        });
+                    
+                }
+            }
+
+            return Table;
         }
 
         public void SetItem(DtoItem item)
