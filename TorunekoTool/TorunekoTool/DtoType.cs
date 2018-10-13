798282372fc48ec46fc24460a8a555eacb2f24a8
mitleidstein <mitleidstein@gmail.com>
2018-10-13 12:49:11 +0900

    new:タイプのDTOであるDtoTypeを作成

---
+++ TorunekoTool/TorunekoTool/DtoType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorunekoTool
+{
+    public class DtoType
+    {
+        public int TypeNumber { get; set; }
+        public string TypeName { get; set; }
+    }
+}
