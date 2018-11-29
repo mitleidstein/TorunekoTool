1d314a126b8263d19ee32b25c2b7b7bb0bad08b2
mitleidstein <mitleidstein@gmail.com>
2018-11-29 20:41:47 +0900

    未識別アイテム用クラスを作成

---
+++ TorunekoTool/TorunekoTool/UnidentifiedItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorunekoTool
+{
+    public class UnidentifiedItem
+    {
+        public string UnidentifiedName { get; set; }
+        public DtoItem Item { get; set; }
+        public int? MoneyToBuy { get; set; }
+        public int? MoneyToSell { get; set; }
+        public int TypeNumber { get; set; }
+    }
+}
