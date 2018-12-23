126d34b128ecacc45d20bcafddd50072bc71716e
mitleidstein <mitleidstein@gmail.com>
2018-12-23 16:04:29 +0900

    feat:ListBoxにフォーカスがあるときに上、下キーが押された際にListBoxの選択値が変わらずにグリッドビューにフォーカスされるようにした

--- TorunekoTool/TorunekoTool/Form1.Designer.cs
+++ TorunekoTool/TorunekoTool/Form1.Designer.cs
@@ -243,7 +243,6 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
-            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             ((System.ComponentModel.ISupportInitialize)(this.DgvMain)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
