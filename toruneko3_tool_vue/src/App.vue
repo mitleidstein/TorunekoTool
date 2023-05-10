a6f422e9c5b846df341ffce06a6a4c6da6dbffdb
m.sampei <m.sampei@cae-sar.co.jp>
2023-05-10 20:27:00 +0900

    機能をある程度実装

--- toruneko3_tool_vue/src/App.vue
+++ toruneko3_tool_vue/src/App.vue
@@ -5,19 +5,50 @@ import unidentifiedItemsCSV from './assets/unidentified_items.json'
 import itemsCSV from './assets/items.json'
 
 const selectedType = ref(0)
+const selectedUnidentifiedItem = ref(null)
+const selectedItem = ref(null)
+
 const unidentifiedItems = ref(unidentifiedItemsCSV)
 const items = ref(itemsCSV)
 const filteredUnidentifiedItems = computed(() => {
   return unidentifiedItems.value.filter((t) => t.item_type_id === selectedType.value)
 })
+
 const filteredItems = computed(() => {
   return items.value
     .filter((t) => t.item_type_id === selectedType.value
       && (!buyingPrice.value || t.buying_price == buyingPrice.value)
-      && (!sellingPrice.value || t.selling_price == sellingPrice.value))
+      && (!sellingPrice.value || t.selling_price == sellingPrice.value)
+      && (Object.keys(indentifiedItems.value.filter((el) => (t.name == el.name))).length == 0)
+    )
+})
+
+const indentifiedItems = computed(() => {
+  return unidentifiedItemsCSV
+      .filter((t) => t.item)
+      .map(t => { return t.item })
 })
 const buyingPrice = ref(null)
 const sellingPrice = ref(null)
+
+function enter() {
+  if (!selectedUnidentifiedItem.value) {
+    return
+  }
+
+  let un = unidentifiedItemsCSV.find(el => el.name == selectedUnidentifiedItem.value.name)
+  if (un) {
+    un.item = selectedItem.value
+    un.buyingPrice = buyingPrice.value
+    un.sellingPrice = sellingPrice.value
+  }
+}
+
+function changedUnidentifiedItem() {
+  selectedItem.value = selectedUnidentifiedItem.value.item
+  buyingPrice.value = selectedUnidentifiedItem.value.buyingPrice
+  sellingPrice.value = selectedUnidentifiedItem.value.sellingPrice
+}
 </script>
 
 <template>
@@ -31,16 +62,16 @@ const sellingPrice = ref(null)
     {{ selectedType }}
     <br/>
     未識別名:
-    <select>
-      <option v-for="item in filteredUnidentifiedItems" :key="item.name" :value="item.id">
+    <select v-model="selectedUnidentifiedItem" @change="changedUnidentifiedItem">
+      <option v-for="item in filteredUnidentifiedItems" :key="item.name" :value="item">
         {{ item.name }}
       </option>
     </select>
     <br/>
     <br/>
-    アイテム選択肢:
-    <select>
-      <option v-for="item in filteredItems" :key="item.name" :value="item.id">
+    識別アイテム:
+    <select v-model="selectedItem">
+      <option v-for="item in filteredItems" :key="item.name" :value="item">
         {{ item.name }}
       </option>
     </select>
@@ -50,6 +81,11 @@ const sellingPrice = ref(null)
     <br/>
     売値:
     <input v-model="sellingPrice" placeholder="売値">
+    
+    <br/>
+    <button @click="enter">
+      決定
+    </button>
   </div>
 </template>
 
