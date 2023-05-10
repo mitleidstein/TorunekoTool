05e8e598de6e2a15464e69b4001b72d1d04f827f
m.sampei <m.sampei@cae-sar.co.jp>
2023-05-10 18:02:49 +0900

    アイテムを表示できるようにした

--- toruneko3_tool_vue/src/App.vue
+++ toruneko3_tool_vue/src/App.vue
@@ -2,12 +2,22 @@
 import { ref, computed } from 'vue'
 import itemTypes from './assets/item_types.json'
 import unidentifiedItemsCSV from './assets/unidentified_items.json'
+import itemsCSV from './assets/items.json'
 
 const selectedType = ref(0)
 const unidentifiedItems = ref(unidentifiedItemsCSV)
+const items = ref(itemsCSV)
 const filteredUnidentifiedItems = computed(() => {
   return unidentifiedItems.value.filter((t) => t.item_type_id === selectedType.value)
 })
+const filteredItems = computed(() => {
+  return items.value
+    .filter((t) => t.item_type_id === selectedType.value
+      && (!buyingPrice.value || t.buying_price == buyingPrice.value)
+      && (!sellingPrice.value || t.selling_price == sellingPrice.value))
+})
+const buyingPrice = ref(null)
+const sellingPrice = ref(null)
 </script>
 
 <template>
@@ -26,6 +36,20 @@ const filteredUnidentifiedItems = computed(() => {
         {{ item.name }}
       </option>
     </select>
+    <br/>
+    <br/>
+    アイテム選択肢:
+    <select>
+      <option v-for="item in filteredItems" :key="item.name" :value="item.id">
+        {{ item.name }}
+      </option>
+    </select>
+    <br/>
+    買値:
+    <input v-model="buyingPrice" placeholder="買値">
+    <br/>
+    売値:
+    <input v-model="sellingPrice" placeholder="売値">
   </div>
 </template>
 
