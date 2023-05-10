b1254036e7a101973d07bb7602a0cd2754fbd28e
m.sampei <m.sampei@cae-sar.co.jp>
2023-05-10 17:37:16 +0900

    未識別名を表示するようにした

--- toruneko3_tool_vue/src/App.vue
+++ toruneko3_tool_vue/src/App.vue
@@ -1,12 +1,30 @@
 <script setup>
-// import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import itemTypes from './assets/item_types.json'
+import unidentifiedItemsCSV from './assets/unidentified_items.json'
+
+const selectedType = ref(0)
+const unidentifiedItems = ref(unidentifiedItemsCSV)
+const filteredUnidentifiedItems = computed(() => {
+  return unidentifiedItems.value.filter((t) => t.item_type_id === selectedType.value)
+})
 </script>
 
 <template>
   <div>
+    アイテム種別:
+    <select v-model="selectedType">
+      <option v-for="type in itemTypes" :key="type.id" :value="type.id">
+        {{ type.name }}
+      </option>
+    </select>
+    {{ selectedType }}
+    <br/>
+    未識別名:
     <select>
-      <option v-for="type in itemTypes" :key="type.id">{{ type.name }}</option>
+      <option v-for="item in filteredUnidentifiedItems" :key="item.name" :value="item.id">
+        {{ item.name }}
+      </option>
     </select>
   </div>
 </template>
